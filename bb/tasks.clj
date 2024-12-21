8b0ba9d3cd282f263d84d49df6813dbaceae11c4
Russell Matney <russell.matney@gmail.com>
2024-12-20 20:17:07 -0500

    wip: starting in on some bb/fs helpers

--- bb/tasks.clj
+++ bb/tasks.clj
@@ -2,13 +2,59 @@
   (:require
    [babashka.fs :as fs]))
 
-(defn post-paths []
+;; vars
+
+(def repo-dir (str (fs/home) "/russmatney/blog"))
+(def posts-dir (str repo-dir "/docs/posts"))
+(def org-garden-dir (str (fs/home) "/todo/garden"))
+(def org-daily-dir (str (fs/home) "/todo/daily"))
+
+
+;; posts
+
+(defn ->post
+  [{:keys [post-path garden-path]
+    :as   post}]
+  (let [path (or post-path garden-path)]
+    (assoc
+      post
+      :fname (fs/file-name path)
+      :last-modified (fs/last-modified-time path)
+      )))
+
+(defn local-post-paths []
+  (->> (concat (fs/glob (str posts-dir) "*.md")
+               (fs/glob (str posts-dir) "**/*.md"))
+       (map str)))
+
+(defn local-posts []
   (->>
-    (fs/glob (str (fs/home) "/todo/garden") "*.org")
-    (sort-by (comp str fs/last-modified-time))
-    reverse ;; TODO lol do this better
-    (take 50)
-    set))
+    (local-post-paths)
+    (map (fn [path] (->post {:post-path (str path)})))))
 
 (comment
-  (post-paths))
+  (local-posts))
+
+;; garden files
+
+(defn garden-paths []
+  (->> (fs/glob (str org-garden-dir) "*.org")
+       (map str)))
+
+(defn garden-posts []
+  (->> (garden-paths)
+       (map (fn [path] (->post {:garden-path path})))))
+
+(defn daily-paths []
+  (->> (fs/glob (str org-daily-dir) "*.org")
+       (map str)))
+
+(defn daily-posts []
+  (->> (daily-paths)
+       (map (fn [path] (->post {:garden-path path})))))
+
+
+(comment
+  (garden-posts)
+  (daily-posts)
+  )
