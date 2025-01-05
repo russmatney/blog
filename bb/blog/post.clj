bce6768446e5dd32c29b87da94bfd4ad77d713dc
Russell Matney <russell.matney@gmail.com>
2025-01-05 17:52:29 -0500

    fix: revert to just :post, write-index rewrite
    Some fixes - nearly back, just the posts page still broken.

--- bb/blog/post.clj
+++ bb/blog/post.clj
@@ -12,7 +12,7 @@
 (defn ->post
   [{:keys [garden-path]
     :as   post}]
-  (let [path                      (:blog.post/path post garden-path)
+  (let [path                      (:post/path post garden-path)
         fname                     (fs/file-name path)
         {title :title date :date} (dates/parse-datey-fname fname)]
     ;; TODO last time git-touched (all times git-touched?)
@@ -20,15 +20,15 @@
     ;; TODO parse dates for non-datey-filenames
     (cond-> post
       true (assoc
-             :blog.post/title title
-             :blog.post/fname fname
-             :blog.post/parent-fname (fs/file-name (fs/parent path))
-             :blog.post/last-modified (fs/file-time->millis (fs/last-modified-time path))
+             :post/title title
+             :post/fname fname
+             :post/parent-fname (fs/file-name (fs/parent path))
+             :post/last-modified (fs/file-time->millis (fs/last-modified-time path))
              )
 
-      (:blog.post/path post) (assoc :blog.post/relative-url
-                                    (string/replace path config/docs-dir ""))
-      (not (nil? date))      (assoc :blog.post/date-created date))
+      (:post/path post) (assoc :post/relative-url
+                               (string/replace path config/docs-dir ""))
+      (not (nil? date)) (assoc :post/date-created date))
     ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -36,23 +36,23 @@
 
 (defn sort-latest-created-first [posts]
   (->> posts
-       (sort-by :blog.post/date-created dates/sort-latest-first)))
+       (sort-by :post/date-created dates/sort-latest-first)))
 
 (defn sort-oldest-created-first [posts]
   (->> posts
-       (sort-by :blog.post/date-created dates/sort-chrono)))
+       (sort-by :post/date-created dates/sort-chrono)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; post getters
 
 (defn posts-in-dir [directory]
   (->> (fs/glob (str directory) "*.md")
-       (map (fn [path] (post/->post {:blog.post/path (str path)})))))
+       (map (fn [path] (->post {:post/path (str path)})))))
 
 (defn local-posts []
   (->> (concat (fs/glob (str config/posts-dir) "*.md")
                (fs/glob (str config/posts-dir) "**/*.md"))
-       (map (fn [path] (post/->post {:blog.post/path (str path)})))
+       (map (fn [path] (->post {:post/path (str path)})))
        ))
 
 (defn hundos-posts []
@@ -65,17 +65,17 @@
 (defn org-root-files []
   (->>
     (fs/glob (str config/org-dir) "*.org")
-    (map (fn [path] (post/->post {:garden-path path})))))
+    (map (fn [path] (->post {:garden-path path})))))
 
 (defn garden-posts []
   (->>
     (fs/glob (str config/org-garden-dir) "*.org")
-    (map (fn [path] (post/->post {:garden-path path})))))
+    (map (fn [path] (->post {:garden-path path})))))
 
 (defn daily-files []
   (->>
     (fs/glob (str config/org-daily-dir) "*.org")
-    (map (fn [path] (post/->post {:garden-path path})))))
+    (map (fn [path] (->post {:garden-path path})))))
 
 (comment
   (org-root-files)
