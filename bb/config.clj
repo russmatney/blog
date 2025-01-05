d6f8078b10f2b2753825f7c555e28d795015befd
Russell Matney <russell.matney@gmail.com>
2025-01-05 15:59:43 -0500

    feat: pull a bunch of defs into a config ns

---
+++ bb/config.clj
@@ -0,0 +1,21 @@
+(ns config
+  (:require
+   [babashka.fs :as fs]
+   )
+  )
+
+
+(def repo-dir (str (fs/home) "/russmatney/blog"))
+(def docs-dir (str repo-dir "/docs"))
+
+(def posts-dir (str repo-dir "/docs/posts"))
+(def hundos-dir (str repo-dir "/docs/posts/100-worders"))
+(def techsposure-dir (str repo-dir "/docs/posts/techsposure"))
+(def getitwrite-dir (str repo-dir "/docs/posts/getitwrite"))
+(def groks-dir (str repo-dir "/docs/posts/groks"))
+
+(def devlogs-dir (str repo-dir "/docs/devlogs"))
+
+(def org-garden-dir (str (fs/home) "/todo/garden"))
+(def org-daily-dir (str (fs/home) "/todo/daily"))
+(def org-dir (str (fs/home) "/todo"))
