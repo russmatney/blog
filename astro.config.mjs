6019299a9d48463142a83e57de539f162ed8727f
Russell Matney <russell.matney@gmail.com>
2026-02-21 12:27:31 -0500

    fix: redirect old hash-based urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -73,6 +73,7 @@ export default defineConfig({
       ],
       components: {
         Footer: './src/components/Footer.astro',
+        Head: './src/components/Head.astro',
       },
     }),
   ],
